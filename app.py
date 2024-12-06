65c5c641e12186f01e3bd8335597b85b57321acc
Aayush6015 <153391642+Aayush6015@users.noreply.github.com>
2024-12-06 10:33:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from scheduler import *
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://bithional:CSZuAvvAc3WEzl2I@cluster.oou5h.mongodb.net/")
+client = MongoClient("mongodb+srv://<username>:<password>@cluster.oou5h.mongodb.net/")
 db = client["price_tracker"]
 products_collection = db["products"]
 
